8cc590420c78fec4b9cd9d2545987209a90e4de6
jeongjoonsup <penny4860@gmail.com>
2018-09-15 20:32:39 +0900

    weights path added

--- __init__.py
+++ __init__.py
@@ -4,4 +4,6 @@ import os
 
 PKG_ROOT = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.dirname(PKG_ROOT)
+DARKNET_WEIGHTS = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "yolo", "darknet53.weights")    
+YOLOV3_WEIGHTS = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "yolo", "yolov3.weights")    
 
