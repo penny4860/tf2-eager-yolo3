0eaef83b072bb84191375389260288e3a72e4074
jeongjoonsup <penny4860@gmail.com>
2018-09-16 17:38:54 +0900

    darknet_keras 에서의 inference 결과 저장

--- exp/debug.py
+++ exp/debug.py
@@ -19,26 +19,29 @@ def get_darknet_keras():
     return model
 
 if __name__ == '__main__':
-#     darknet = get_darknet_keras()
-#     weights = darknet.layers[1].get_weights()[0]
-#     import numpy as np
-#     np.save("conv0", weights)
-    
-    import tensorflow as tf
-    tf.enable_eager_execution()
-    darknet_eager = get_darknet_eager()
-    weights_eager = darknet_eager.get_variables(0, "kernel")[0]
     import numpy as np
-    np.save("conv0_eager", weights_eager.numpy())
-
-    conv0 = np.load("conv0.npy")
-    conv0_eager = weights_eager.numpy()
-
-    print(conv0[0,0,0,0:4])
-    print(conv0_eager[0,0,0,0:4])
-
-#     [ 0.03592291 -0.03602274 -0.03412793  0.03506648]
-#     [ 0.03353848  0.00134301 -0.02568628 -0.26286137]
+    np.random.seed(0)
+    images = np.random.randn(1, 224, 224, 3)
+    darknet = get_darknet_keras()
+    ys = darknet.predict(images)
+    print(ys.shape)
+    np.save("ys_keras", ys)
+    
+    
+#     
+#     import tensorflow as tf
+#     tf.enable_eager_execution()
+#     darknet_eager = get_darknet_eager()
+#     weights_eager = darknet_eager.get_variables(0, "kernel")[0]
+#     import numpy as np
+#     np.save("conv0_eager", weights_eager.numpy())
+# 
+#     conv0 = np.load("conv0.npy")
+#     conv0_eager = weights_eager.numpy()
+# 
+#     print(np.allclose(conv0, conv0_eager))
+# #     [ 0.03592291 -0.03602274 -0.03412793  0.03506648]
+# #     [ 0.03353848  0.00134301 -0.02568628 -0.26286137]
 
 
 
