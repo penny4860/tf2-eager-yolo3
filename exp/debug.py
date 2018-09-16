d1836d9d10d775c5dd30aad2089b7add0da03bb1
jeongjoonsup <penny4860@gmail.com>
2018-09-16 17:54:31 +0900

    darknet53_conv 구현 검증

--- exp/debug.py
+++ exp/debug.py
@@ -22,24 +22,20 @@ if __name__ == '__main__':
     import numpy as np
     np.random.seed(0)
     images = np.random.randn(1, 224, 224, 3)
-    darknet = get_darknet_keras()
-    ys = darknet.predict(images)
-    print(ys.shape)
-    np.save("ys_keras", ys)
-    
-    
-#     
-#     import tensorflow as tf
-#     tf.enable_eager_execution()
-#     darknet_eager = get_darknet_eager()
-#     weights_eager = darknet_eager.get_variables(0, "kernel")[0]
-#     import numpy as np
-#     np.save("conv0_eager", weights_eager.numpy())
-# 
-#     conv0 = np.load("conv0.npy")
-#     conv0_eager = weights_eager.numpy()
-# 
-#     print(np.allclose(conv0, conv0_eager))
+
+#     darknet = get_darknet_keras()
+#     ys = darknet.predict(images)
+#     print(ys.shape)
+#     np.save("ys_keras", ys)
+
+    import tensorflow as tf
+    tf.enable_eager_execution()
+    darknet_eager = get_darknet_eager()
+    ys = darknet_eager(tf.constant(images.astype(np.float32)))
+    ys_eager = ys.numpy()
+    ys_keras = np.load("ys_keras.npy")
+    print(np.allclose(ys_eager, ys_keras))
+
 # #     [ 0.03592291 -0.03602274 -0.03412793  0.03506648]
 # #     [ 0.03353848  0.00134301 -0.02568628 -0.26286137]
 
