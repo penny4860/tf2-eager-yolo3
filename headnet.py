d18daef99ead55675e6b84e019484d33e1f44eaa
jeongjoonsup <penny4860@gmail.com>
2018-09-17 20:44:43 +0900

    headnet module 완성

--- headnet.py
+++ headnet.py
@@ -8,15 +8,42 @@ models = tf.keras.models
 
 # Darknet53 feature extractor
 class Headnet(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, n_features=255):
         super(Headnet, self).__init__(name='')
-#         # self.l5d = _ResidualBlock([512, 1024], layer_idx=[50, 51], name="stage5")
-#         self.num_layers = 52
-#         self._init_vars()
+        
+        self.stage5_conv5 = _Conv5([512, 1024, 512, 1024, 512],
+                                   [75, 76, 77, 78, 79])
+        self.stage5_conv2 = _Conv2([1024, n_features],
+                                   [80, 81])
+        self.stage5_upsampling = _Upsamling([256], [84])
 
-    def call(self, input_tensor, training=False):
-#         x = self.l0a(input_tensor, training)
-        pass
+        self.stage4_conv5 = _Conv5([256, 512, 256, 512, 256],
+                                   [87, 88, 89, 90, 91])
+        self.stage4_conv2 = _Conv2([512, n_features],
+                                   [92, 93])
+        self.stage4_upsampling = _Upsamling([128], [96])
+
+        self.stage3_conv5 = _Conv5([128, 256, 128, 256, 128],
+                                   [99, 100, 101, 102, 103])
+        self.stage3_conv2 = _Conv2([1024, n_features],
+                                   [104, 105])
+        self.num_layers = 106
+        self._init_vars()
+
+    def call(self, stage3_in, stage4_in, stage5_in, training=False):
+        x = self.stage5_conv5(stage5_in, training)
+        stage5_output = self.stage5_conv2(x, training)
+
+        x = self.stage5_upsampling(x, training)
+        x = layers.concatenate([x, stage4_in])
+        x = self.stage4_conv5(x, training)
+        stage4_output = self.stage4_conv2(x, training)
+
+        x = self.stage4_upsampling(x, training)
+        x = layers.concatenate([x, stage3_in])
+        x = self.stage3_conv5(x, training)
+        stage3_output = self.stage3_conv2(x, training)
+        return stage5_output, stage4_output, stage3_output
 
     def get_variables(self, layer_idx, suffix=None):
         if suffix:
@@ -31,9 +58,10 @@ class Headnet(tf.keras.Model):
 
     def _init_vars(self):
         import numpy as np
-        imgs = np.random.randn(1, 256, 256, 3).astype(np.float32)
-        input_tensor = tf.constant(imgs)
-        self.call(input_tensor)
+        s3 = tf.constant(np.random.randn(1, 32, 32, 256).astype(np.float32))
+        s4 = tf.constant(np.random.randn(1, 16, 16, 512).astype(np.float32))
+        s5 = tf.constant(np.random.randn(1, 8, 8, 1024).astype(np.float32))
+        return self.call(s3, s4, s5, training=False)
 
 
 class _Conv5(tf.keras.Model):
@@ -91,13 +119,13 @@ class _Conv2(tf.keras.Model):
         x = self.conv1(input_tensor)
         x = self.bn(x, training=training)
         x = tf.nn.leaky_relu(x, alpha=0.1)
-        x = self.conv2(input_tensor, training)
+        x = self.conv2(input_tensor)
         return x
 
 
 class _Upsamling(tf.keras.Model):
     def __init__(self, filters, layer_idx, name=""):
-        super(_Conv2, self).__init__(name=name)
+        super(_Upsamling, self).__init__(name=name)
         
         layer_names = ["layer_{}".format(i) for i in layer_idx]
 
@@ -116,11 +144,12 @@ class _Upsamling(tf.keras.Model):
 if __name__ == '__main__':
     import numpy as np
     tf.enable_eager_execution()
-    imgs = np.random.randn(1, 256, 256, 3).astype(np.float32)
-    input_tensor = tf.constant(imgs)
+    s3 = tf.constant(np.random.randn(1, 32, 32, 256).astype(np.float32))
+    s4 = tf.constant(np.random.randn(1, 16, 16, 512).astype(np.float32))
+    s5 = tf.constant(np.random.randn(1, 8, 8, 1024).astype(np.float32))
     
     # (1, 256, 256, 3) => (1, 8, 8, 1024)
-    bodynet = Bodynet()
-    s3, s4, s5 = bodynet(input_tensor)
-    print(s3.shape, s4.shape, s5.shape)
+    headnet = Headnet()
+    f5, f4, f3 = headnet(s3, s4, s5)
+    print(f5.shape, f4.shape, f3.shape)
 
