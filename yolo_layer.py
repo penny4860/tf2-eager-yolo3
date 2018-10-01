f4009215a1ded4188048a00908ec9e9be4ee6c5e
jeongjoonsup <penny4860@gmail.com>
2018-10-01 21:15:43 +0900

    YoloLayerNp class added

--- yolo_layer.py
+++ yolo_layer.py
@@ -3,26 +3,50 @@
 import tensorflow as tf
 import numpy as np
 
-
 def cell_grid(max_grid, batch_size=2):
 
     max_grid_h, max_grid_w = max_grid
-
+    
     cell_x = np.arange(max_grid_w)
     cell_x = np.tile(cell_x, [max_grid_h])
     cell_x = np.reshape(cell_x, (1, max_grid_h, max_grid_w, 1, 1))
     cell_x = cell_x.astype(np.float32)
-    
     cell_y = np.transpose(cell_x, (0,2,1,3,4))
     cell_grid = np.tile(np.concatenate([cell_x,cell_y],-1), [batch_size, 1, 1, 3, 1])
     return cell_grid
+
+class YoloLayerNp(object):
+    def __init__(self,
+                 anchors=[90, 95, 92, 154, 139, 281],
+                 max_grid=[288, 288], 
+                 batch_size=2,
+                 warmup_batches=0,
+                 ignore_thresh=0.5, 
+                 grid_scale=1,
+                 obj_scale=5,
+                 noobj_scale=1,
+                 xywh_scale=1,
+                 class_scale=1):
+        # make the model settings persistent
+        self.ignore_thresh  = ignore_thresh
+        self.warmup_batches = warmup_batches
+        self.anchors        = tf.constant(anchors, dtype='float', shape=[1,1,1,3,2])
+        self.grid_scale     = grid_scale
+        self.obj_scale      = obj_scale
+        self.noobj_scale    = noobj_scale
+        self.xywh_scale     = xywh_scale
+        self.class_scale    = class_scale        
+
+        # make a persistent mesh grid
+        self.cell_grid = cell_grid(max_grid, batch_size)
+
 
 
 from yolo_ import YoloLayer
 if __name__ == '__main__':
     
-    BATCH_SIZE = 2
-    MAX_GRID = [288, 288]
+    BATCH_SIZE = 1
+    MAX_GRID = [3, 3]
     
     cell_grid_tensor = YoloLayer(max_grid=MAX_GRID, batch_size=BATCH_SIZE).cell_grid
     
@@ -31,6 +55,5 @@ if __name__ == '__main__':
     cell_grid = cell_grid(MAX_GRID, batch_size=BATCH_SIZE)
 
     print(np.allclose(cell_grid_value, cell_grid))
-
 
 
