9322a6eb6512d320dd905cadbcdec9852a467dc5
jeongjoonsup <penny4860@gmail.com>
2018-10-03 16:09:15 +0900

    utils error fix

--- loss/utils.py
+++ loss/utils.py
@@ -146,7 +146,7 @@ def reshape_y_pred_tensor(y_pred):
     y_pred = tf.reshape(y_pred, tf.concat([tf.shape(y_pred)[:3], tf.constant([3, -1])], axis=0))
     return y_pred
 
-def setup_env_tensor(input_image, y_true):
+def setup_env_tensor(y_true):
     # initialize the masks
     object_mask     = tf.expand_dims(y_true[..., 4], 4)
 
@@ -154,11 +154,7 @@ def setup_env_tensor(input_image, y_true):
     grid_h      = tf.shape(y_true)[1]
     grid_w      = tf.shape(y_true)[2]
     grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1,1,1,1,2])
-
-    net_h       = tf.shape(input_image)[1]
-    net_w       = tf.shape(input_image)[2]            
-    net_factor  = tf.reshape(tf.cast([net_w, net_h], tf.float32), [1,1,1,1,2])
-    return object_mask, grid_factor, net_factor, grid_h, grid_w
+    return object_mask, grid_factor, grid_h, grid_w
 
 def adjust_pred_tensor(y_pred, cell_grid, grid_h, grid_w):
     pred_box_xy    = (cell_grid[:,:grid_h,:grid_w,:,:] + tf.sigmoid(y_pred[..., :2]))  # sigma(t_xy) + c_xy
