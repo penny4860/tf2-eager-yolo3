38aa7c15bcb062808295d47af3e0e3823f25fe0e
jeongjoonsup <penny4860@gmail.com>
2018-10-03 15:59:08 +0900

    yolo.loss.calc_tensor added

---
+++ loss/calc_tensor.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from yolo.loss.utils import adjust_pred_tensor, adjust_true_tensor
+from yolo.loss.utils import conf_delta_tensor, intersect_areas_tensor, reshape_y_pred_tensor, setup_env_tensor
+from yolo.loss.utils import loss_class_tensor, loss_conf_tensor, loss_wh_tensor, loss_xy_tensor, wh_scale_tensor
+
+
+class LossTensorCalculator(object):
+    def __init__(self,
+                 anchors=[90, 95, 92, 154, 139, 281],
+                 max_grid=[288, 288], 
+                 batch_size=2,
+                 warmup_batches=0,
+                 ignore_thresh=0.5, 
+                 grid_scale=1,
+                 obj_scale=5,
+                 noobj_scale=1,
+                 xywh_scale=1,
+                 class_scale=1):
+        self.ignore_thresh  = ignore_thresh
+        self.warmup_batches = warmup_batches
+        self.anchors        = tf.constant(anchors, dtype='float', shape=[1,1,1,3,2])
+        self.grid_scale     = grid_scale
+        self.obj_scale      = obj_scale
+        self.noobj_scale    = noobj_scale
+        self.xywh_scale     = xywh_scale
+        self.class_scale    = class_scale        
+
+        # make a persistent mesh grid
+        max_grid_h, max_grid_w = max_grid
+
+        cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(max_grid_w), [max_grid_h]), (1, max_grid_h, max_grid_w, 1, 1)))
+        cell_y = tf.transpose(cell_x, (0,2,1,3,4))
+        self.cell_grid = tf.tile(tf.concat([cell_x,cell_y],-1), [batch_size, 1, 1, 3, 1])
+
+    def call(self, x):
+        input_image, y_pred, y_true, true_boxes = x
+        
+        # 1. setup
+        y_pred = reshape_y_pred_tensor(y_pred)
+        object_mask, grid_factor, net_factor, grid_h, grid_w = setup_env_tensor(input_image, y_true)
+
+        # 2. Adjust prediction
+        pred_box_xy, pred_box_wh, pred_box_conf, pred_box_class = adjust_pred_tensor(y_pred, self.cell_grid, grid_h, grid_w)
+
+        # 3. Adjust ground truth
+        true_box_xy, true_box_wh, true_box_conf, true_box_class = adjust_true_tensor(y_true)
+
+        # 4. conf_delta tensor
+        intersect_areas, pred_areas, true_areas = intersect_areas_tensor(true_boxes,
+                                                                         pred_box_xy,
+                                                                         pred_box_wh,
+                                                                         grid_factor,
+                                                                         net_factor,
+                                                                         self.anchors)
+
+        conf_delta = conf_delta_tensor(pred_box_conf, intersect_areas, pred_areas, true_areas, self.ignore_thresh)
+
+        # 5. loss tensor
+        wh_scale =  wh_scale_tensor(true_box_wh, self.anchors, net_factor)
+
+        loss_xy = loss_xy_tensor(object_mask, pred_box_xy, true_box_xy, wh_scale, self.xywh_scale)
+        loss_wh = loss_wh_tensor(object_mask, pred_box_wh, true_box_wh, wh_scale, self.xywh_scale)
+        loss_conf = loss_conf_tensor(object_mask, pred_box_conf, true_box_conf, self.obj_scale, self.noobj_scale, conf_delta)
+        loss_class = loss_class_tensor(object_mask, pred_box_class, true_box_class, self.class_scale)
+        loss = loss_xy + loss_wh + loss_conf + loss_class
+        return loss*self.grid_scale
+
--- loss/utils.py
+++ loss/utils.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from scipy.special import expit as sigmoid
 import numpy as np
 import tensorflow as tf
@@ -141,3 +140,102 @@ def cell_grid(max_grid, batch_size=2):
     cell_y = np.transpose(cell_x, (0,2,1,3,4))
     cell_grid = np.tile(np.concatenate([cell_x,cell_y],-1), [batch_size, 1, 1, 3, 1])
     return cell_grid
+
+def reshape_y_pred_tensor(y_pred):
+    # adjust the shape of the y_predict [batch, grid_h, grid_w, 3, 4+1+nb_class]
+    y_pred = tf.reshape(y_pred, tf.concat([tf.shape(y_pred)[:3], tf.constant([3, -1])], axis=0))
+    return y_pred
+
+def setup_env_tensor(input_image, y_true):
+    # initialize the masks
+    object_mask     = tf.expand_dims(y_true[..., 4], 4)
+
+    # compute grid factor and net factor
+    grid_h      = tf.shape(y_true)[1]
+    grid_w      = tf.shape(y_true)[2]
+    grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1,1,1,1,2])
+
+    net_h       = tf.shape(input_image)[1]
+    net_w       = tf.shape(input_image)[2]            
+    net_factor  = tf.reshape(tf.cast([net_w, net_h], tf.float32), [1,1,1,1,2])
+    return object_mask, grid_factor, net_factor, grid_h, grid_w
+
+def adjust_pred_tensor(y_pred, cell_grid, grid_h, grid_w):
+    pred_box_xy    = (cell_grid[:,:grid_h,:grid_w,:,:] + tf.sigmoid(y_pred[..., :2]))  # sigma(t_xy) + c_xy
+    pred_box_wh    = y_pred[..., 2:4]                                                       # t_wh
+    pred_box_conf  = tf.expand_dims(tf.sigmoid(y_pred[..., 4]), 4)                          # adjust confidence
+    pred_box_class = y_pred[..., 5:]                                                        # adjust class probabilities      
+    return pred_box_xy, pred_box_wh, pred_box_conf, pred_box_class
+
+def adjust_true_tensor(y_true):
+    true_box_xy    = y_true[..., 0:2] # (sigma(t_xy) + c_xy)
+    true_box_wh    = y_true[..., 2:4] # t_wh
+    true_box_conf  = tf.expand_dims(y_true[..., 4], 4)
+    true_box_class = tf.argmax(y_true[..., 5:], -1)
+    return true_box_xy, true_box_wh, true_box_conf, true_box_class
+
+def intersect_areas_tensor(true_boxes, pred_box_xy, pred_box_wh, grid_factor, net_factor, anchors):
+    
+    # then, ignore the boxes which have good overlap with some true box
+    true_xy = true_boxes[..., 0:2] / grid_factor
+    true_wh = true_boxes[..., 2:4] / net_factor
+     
+    true_wh_half = true_wh / 2.
+    true_mins    = true_xy - true_wh_half
+    true_maxes   = true_xy + true_wh_half
+     
+    pred_xy = tf.expand_dims(pred_box_xy / grid_factor, 4)
+    pred_wh = tf.expand_dims(tf.exp(pred_box_wh) * anchors / net_factor, 4)
+     
+    pred_wh_half = pred_wh / 2.
+    pred_mins    = pred_xy - pred_wh_half
+    pred_maxes   = pred_xy + pred_wh_half    
+    
+    intersect_mins  = tf.maximum(pred_mins,  true_mins)
+    intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+    
+    intersect_wh    = tf.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+
+    true_areas = true_wh[..., 0] * true_wh[..., 1]
+    pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+    return intersect_areas, pred_areas, true_areas
+
+def conf_delta_tensor(pred_box_conf, intersect_areas, pred_areas, true_areas, ignore_thresh):
+    # initially, drag all objectness of all boxes to 0
+    conf_delta  = pred_box_conf - 0 
+
+    union_areas = pred_areas + true_areas - intersect_areas
+    iou_scores  = tf.truediv(intersect_areas, union_areas)
+     
+    best_ious   = tf.reduce_max(iou_scores, axis=4)        
+    conf_delta *= tf.expand_dims(tf.to_float(best_ious < ignore_thresh), 4)
+    return conf_delta
+
+
+def wh_scale_tensor(true_box_wh, anchors, net_factor):
+    wh_scale = tf.exp(true_box_wh) * anchors / net_factor
+    wh_scale = tf.expand_dims(2 - wh_scale[..., 0] * wh_scale[..., 1], axis=4) # the smaller the box, the bigger the scale
+    return wh_scale
+
+def loss_xy_tensor(object_mask, pred_box_xy, true_box_xy, wh_scale, xywh_scale):
+    xy_delta    = object_mask   * (pred_box_xy-true_box_xy) * wh_scale * xywh_scale
+    loss_xy    = tf.reduce_sum(tf.square(xy_delta),       list(range(1,5)))
+    return loss_xy
+
+def loss_wh_tensor(object_mask, pred_box_wh, true_box_wh, wh_scale, xywh_scale):
+    wh_delta    = object_mask   * (pred_box_wh-true_box_wh) * wh_scale * xywh_scale
+    loss_wh    = tf.reduce_sum(tf.square(wh_delta),       list(range(1,5)))
+    return loss_wh
+    
+def loss_conf_tensor(object_mask, pred_box_conf, true_box_conf, obj_scale, noobj_scale, conf_delta):
+    conf_delta  = object_mask * (pred_box_conf-true_box_conf) * obj_scale + (1-object_mask) * conf_delta * noobj_scale
+    loss_conf  = tf.reduce_sum(tf.square(conf_delta),     list(range(1,5)))
+    return loss_conf
+
+def loss_class_tensor(object_mask, pred_box_class, true_box_class, class_scale):
+    class_delta = object_mask * \
+                  tf.expand_dims(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class, logits=pred_box_class), 4) * \
+                  class_scale
+    loss_class = tf.reduce_sum(class_delta,               list(range(1,5)))
+    return loss_class
