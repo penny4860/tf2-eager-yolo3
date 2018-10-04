7a989b60ed3116804dcc417b2def933f40f72bfd
jeongjoonsup <penny4860@gmail.com>
2018-10-04 19:42:32 +0900

    code cleaning

--- train.py
+++ train.py
@@ -2,39 +2,9 @@
 
 import tensorflow as tf
 tf.enable_eager_execution()
-import numpy as np
 
 from yolo.yolonet import Yolonet
 from yolo.loss import loss_fn
-
-
-from yolo import PROJECT_ROOT
-import os
-TEST_SAMPLE_ROOT = os.path.join(PROJECT_ROOT, "tests", "samples")
-def _list_y_trues():
-    # (2, 9, 9, 3, 6) (2, 18, 18, 3, 6) (2, 36, 36, 3, 6)
-    y_true_1 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_1.npy")).astype(np.float32)
-    y_true_2 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_2.npy")).astype(np.float32)
-    y_true_3 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_3.npy")).astype(np.float32)
-    y_trues = [y_true_1, y_true_2, y_true_3]
-    return y_trues
-
-def _list_y_preds():
-    # (2, 9, 9, 3, 6) (2, 18, 18, 3, 6) (2, 36, 36, 3, 6)
-    y_pred_1 = np.load(os.path.join(TEST_SAMPLE_ROOT, "y_pred_1.npy")).astype(np.float32)
-    y_pred_2 = np.load(os.path.join(TEST_SAMPLE_ROOT, "y_pred_2.npy")).astype(np.float32)
-    y_pred_3 = np.load(os.path.join(TEST_SAMPLE_ROOT, "y_pred_3.npy")).astype(np.float32)
-    ys_preds = [y_pred_1, y_pred_2, y_pred_3]
-    return ys_preds
-
-def _true_boxes():
-    # (2, 1, 1, 1, 2, 4)
-    true_boxes = np.load(os.path.join(TEST_SAMPLE_ROOT, "t_batch.npy")).astype(np.float32)
-    return true_boxes
-
-def _images():
-    images = np.load(os.path.join(TEST_SAMPLE_ROOT, "x_batch.npy")).astype(np.float32)
-    return images
 
 
 def train(images_tensor, list_y_trues, true_boxes, optimizer, model, num_epoches=500, verbose=10):
@@ -55,6 +25,27 @@ def train(images_tensor, list_y_trues, true_boxes, optimizer, model, num_epoches
 
 
 if __name__ == '__main__':
+    
+    from yolo import PROJECT_ROOT
+    import os
+    import numpy as np
+    TEST_SAMPLE_ROOT = os.path.join(PROJECT_ROOT, "tests", "samples")
+    def _list_y_trues():
+        # (2, 9, 9, 3, 6) (2, 18, 18, 3, 6) (2, 36, 36, 3, 6)
+        y_true_1 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_1.npy")).astype(np.float32)
+        y_true_2 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_2.npy")).astype(np.float32)
+        y_true_3 = np.load(os.path.join(TEST_SAMPLE_ROOT, "yolo_3.npy")).astype(np.float32)
+        y_trues = [y_true_1, y_true_2, y_true_3]
+        return y_trues
+    
+    def _true_boxes():
+        # (2, 1, 1, 1, 2, 4)
+        true_boxes = np.load(os.path.join(TEST_SAMPLE_ROOT, "t_batch.npy")).astype(np.float32)
+        return true_boxes
+    
+    def _images():
+        images = np.load(os.path.join(TEST_SAMPLE_ROOT, "x_batch.npy")).astype(np.float32)
+        return images
     
     # 1. setup dataset
     images_tensor = tf.constant(_images())
