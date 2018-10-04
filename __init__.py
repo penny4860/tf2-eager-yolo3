837aa058d762596818ec7fd2c53fda3b050ddedb
jeongjoonsup <penny4860@gmail.com>
2018-10-05 01:23:51 +0900

    yolov3.h5 path added

--- __init__.py
+++ __init__.py
@@ -6,4 +6,5 @@ PKG_ROOT = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.dirname(PKG_ROOT)
 DARKNET_WEIGHTS = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "yolo", "darknet53_conv.weights")    
 YOLOV3_WEIGHTS = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "yolo", "yolov3.weights")    
+YOLOV3_H5 = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "yolo", "yolov3.h5")    
 
