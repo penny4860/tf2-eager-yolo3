b54fb54f87cca56caea630fd13107eb206bc0fe2
jeongjoonsup <penny4860@gmail.com>
2018-10-25 21:29:34 +0900

    check_labels 스크립트 추가

---
+++ check_labels.py
@@ -0,0 +1,26 @@
+
+
+import glob
+import os
+
+from yolo import PROJECT_ROOT
+from yolo.dataset.annotation import PascalVocXmlParser
+
+def get_unique_labels(files):
+    parser = PascalVocXmlParser()
+    labels = []
+    for fname in files:
+        labels += parser.get_labels(fname)
+        labels = list(set(labels))
+    labels.sort()
+    return labels
+
+if __name__ == '__main__':
+    ann_root = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "udacity", "anns")
+    
+    # 1. create generator
+    train_ann_fnames = glob.glob(os.path.join(ann_root, "*.xml"))
+    print(get_unique_labels(train_ann_fnames))
+    print(len(train_ann_fnames))
+
+
