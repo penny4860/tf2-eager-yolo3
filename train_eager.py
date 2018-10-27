7bfbd652dcd47e774480fc5c16f6001f2402ba1f
jeongjoonsup <penny4860@gmail.com>
2018-10-27 09:47:58 +0900

    load pretrained weights

--- train_eager.py
+++ train_eager.py
@@ -65,7 +65,8 @@ if __name__ == '__main__':
     
     # 2. create model
     model = Yolonet(n_classes=len(config["model"]["labels"]))
-    model.load_darknet_params(config["pretrained"]["darknet_format"], skip_detect_layer=True)
+    model.load_weights(os.path.join(config["train"]["save_folder"], "weights.h5"))
+    # model.load_darknet_params(config["pretrained"]["darknet_format"], skip_detect_layer=True)
  
     # 4. traini
     train_fn(model,
