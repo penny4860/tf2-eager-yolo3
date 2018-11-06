34ea648afa1e1b60c03010421d7694fdbf2e7d6c
jeongjoonsup <penny4860@gmail.com>
2018-11-07 08:17:15 +0900

    check kitti labels

--- check_labels.py
+++ check_labels.py
@@ -16,11 +16,13 @@ def get_unique_labels(files):
     return labels
 
 if __name__ == '__main__':
-    ann_root = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "udacity", "anns")
+    ann_root = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "kitti", "anns")
     
     # 1. create generator
     train_ann_fnames = glob.glob(os.path.join(ann_root, "*.xml"))
     print(get_unique_labels(train_ann_fnames))
     print(len(train_ann_fnames))
+    # ['Cyclist', 'DontCare', 'Misc', 'Person_sitting', 'Tram', 'Truck', 'Van', 'car', 'person']
+    # 7481
 
 
