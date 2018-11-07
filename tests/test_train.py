2f35f061549c16ea4bdab5a1fa2835ac692baa7f
jeongjoonsup <penny4860@gmail.com>
2018-11-08 04:51:48 +0900

    train_fn() 에서 valid_generator 가 None 이면 train_loss 만

--- tests/test_train.py
+++ tests/test_train.py
@@ -17,12 +17,6 @@ def test_train(setup_tf_eager, setup_darknet_weights, setup_train_dirs):
                                  min_net_size=288,
                                  max_net_size=288,    
                                  jitter=False)
-    valid_generator = BatchGenerator(ann_fnames, image_root,
-                                       batch_size=2,
-                                       labels=["raccoon"],
-                                       min_net_size=288,
-                                       max_net_size=288,    
-                                       jitter=False)
  
     # 2. create model
     model = Yolonet(n_classes=1)
@@ -31,7 +25,7 @@ def test_train(setup_tf_eager, setup_darknet_weights, setup_train_dirs):
     # 3. training
     loss_history = train_fn(model,
                             generator,
-                            valid_generator,
+                            valid_generator=None,
                             num_epoches=3)
     assert loss_history[0] > loss_history[-1]
 
