70b6dcd74c35b9e7af986366a0ba1dc6edabd029
jeongjoonsup <penny4860@gmail.com>
2018-11-08 05:04:39 +0900

    test_train cleaning

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from yolo.net.yolonet import Yolonet
 from yolo.train import train_fn
 from yolo.dataset.generator import BatchGenerator
 
 
-def test_overfit(setup_tf_eager, setup_darknet_weights, setup_train_dirs):
-    
+@pytest.fixture(scope='function')
+def setup_generator(setup_train_dirs):
     ann_fnames, image_root = setup_train_dirs
-    darknet_weights = setup_darknet_weights
-
-    # 1. create generator
     generator = BatchGenerator(ann_fnames, image_root,
                                  batch_size=2,
                                  labels=["raccoon"],
                                  min_net_size=288,
                                  max_net_size=288,    
                                  jitter=False)
+    return generator
+
+
+def test_overfit(setup_tf_eager, setup_darknet_weights, setup_generator):
+    
+    # 1. create generator
+    generator = setup_generator
  
     # 2. create model
     model = Yolonet(n_classes=1)
-    model.load_darknet_params(darknet_weights, True)
+    model.load_darknet_params(setup_darknet_weights, True)
      
     # 3. training
     loss_history = train_fn(model,
@@ -30,38 +36,23 @@ def test_overfit(setup_tf_eager, setup_darknet_weights, setup_train_dirs):
     assert loss_history[0] > loss_history[-1]
 
 
-def test_train(setup_tf_eager, setup_darknet_weights, setup_train_dirs):
-    
-    ann_fnames, image_root = setup_train_dirs
-    darknet_weights = setup_darknet_weights
+def test_train(setup_tf_eager, setup_darknet_weights, setup_generator):
 
     # 1. create generator
-    generator = BatchGenerator(ann_fnames, image_root,
-                                 batch_size=2,
-                                 labels=["raccoon"],
-                                 min_net_size=288,
-                                 max_net_size=288,    
-                                 jitter=False)
-    valid_generator = BatchGenerator(ann_fnames, image_root,
-                                       batch_size=2,
-                                       labels=["raccoon"],
-                                       min_net_size=288,
-                                       max_net_size=288,    
-                                       jitter=False)
+    generator = setup_generator
  
     # 2. create model
     model = Yolonet(n_classes=1)
-    model.load_darknet_params(darknet_weights, True)
+    model.load_darknet_params(setup_darknet_weights, True)
      
     # 3. training
     loss_history = train_fn(model,
                             generator,
-                            valid_generator,
+                            valid_generator=setup_generator,
                             num_epoches=3)
     assert loss_history[0] > loss_history[-1]
+    
 
-
-import pytest
 if __name__ == '__main__':
     pytest.main([__file__, "-v", "-s"])
  
