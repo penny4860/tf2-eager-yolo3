2a9fba2f17509b5eaa2ad33bf7ebdfcbb9549a3d
jeongjoonsup <penny4860@gmail.com>
2018-11-09 02:32:10 +0900

    Evaluator를 frontend module로 이동

--- eval.py
+++ eval.py
@@ -3,13 +3,7 @@
 import tensorflow as tf
 tf.enable_eager_execution()
 
-import cv2
-import os
 import argparse
-import json
-from tqdm import tqdm
-
-from yolo.utils.box import draw_boxes
 
 argparser = argparse.ArgumentParser(
     description='evaluate yolo-v3 network')
@@ -32,38 +26,9 @@ argparser.add_argument(
     default=0.5)
 
 
-import numpy as np
-from yolo.dataset.annotation import parse_annotation
-from yolo.eval.fscore import count_true_positives, calc_score
-class Evaluator(object):
-    def __init__(self, detector):
-        self._detector = detector
-    
-    def run(self, ann_fnames, img_dname, class_labels, threshold=0.5, save_dname=None):
-        n_true_positives = 0
-        n_truth = 0
-        n_pred = 0
-        for ann_fname in tqdm(ann_fnames):
-            img_fname, true_boxes, true_labels = parse_annotation(ann_fname, img_dname, class_labels)
-            true_labels = np.array(true_labels)
-            image = cv2.imread(img_fname)[:,:,::-1]
-    
-            boxes, labels, probs = self._detector.detect(image, threshold)
-            
-            n_true_positives += count_true_positives(boxes, true_boxes, labels, true_labels)
-            n_truth += len(true_boxes)
-            n_pred += len(boxes)
-            
-            if save_dname:
-                if not os.path.exists(save_dname):
-                    os.makedirs(save_dname)
-                image_ = draw_boxes(image, boxes, labels, probs, class_labels, desired_size=416)
-                output_path = os.path.join(save_dname, os.path.split(img_fname)[-1])
-                cv2.imwrite(output_path, image_[:,:,::-1])
-        return calc_score(n_true_positives, n_truth, n_pred)
-
 if __name__ == '__main__':
     from yolo.config import ConfigParser
+    from yolo.frontend import Evaluator
     args = argparser.parse_args()
     config_parser = ConfigParser(args.config)
     model = config_parser.create_model()
--- yolo/frontend.py
+++ yolo/frontend.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import cv2
+import os
+import numpy as np
+from tqdm import tqdm
+
 from yolo.post_proc.decoder import postprocess_ouput
 from yolo.net.yolonet import preprocess_input
-from yolo.utils.box import boxes_to_array, to_minmax
-import numpy as np
+from yolo.utils.box import boxes_to_array, to_minmax, draw_boxes
+from yolo.dataset.annotation import parse_annotation
+from yolo.eval.fscore import count_true_positives, calc_score
+
 
 class YoloDetector(object):
     
@@ -42,5 +49,32 @@ class YoloDetector(object):
             boxes, labels, probs = [], [], []
         return boxes, labels, probs
 
+
+class Evaluator(object):
+    def __init__(self, yolo_detector):
+        self._detector = yolo_detector
+    
+    def run(self, ann_fnames, img_dname, class_labels, threshold=0.5, save_dname=None):
+        n_true_positives = 0
+        n_truth = 0
+        n_pred = 0
+        for ann_fname in tqdm(ann_fnames):
+            img_fname, true_boxes, true_labels = parse_annotation(ann_fname, img_dname, class_labels)
+            true_labels = np.array(true_labels)
+            image = cv2.imread(img_fname)[:,:,::-1]
+    
+            boxes, labels, probs = self._detector.detect(image, threshold)
+            
+            n_true_positives += count_true_positives(boxes, true_boxes, labels, true_labels)
+            n_truth += len(true_boxes)
+            n_pred += len(boxes)
+            
+            if save_dname:
+                if not os.path.exists(save_dname):
+                    os.makedirs(save_dname)
+                image_ = draw_boxes(image, boxes, labels, probs, class_labels, desired_size=416)
+                output_path = os.path.join(save_dname, os.path.split(img_fname)[-1])
+                cv2.imwrite(output_path, image_[:,:,::-1])
+        return calc_score(n_true_positives, n_truth, n_pred)
 
 
